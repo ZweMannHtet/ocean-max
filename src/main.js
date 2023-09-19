fac8faf17af3c6517afaf227655f1a225b4c0419
ZweMannHtet <zweman98240@gmail.com>
2023-09-20 03:16:34 +0630

    Setup fontawsome and import some icons

--- src/main.js
+++ src/main.js
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
+import { faTruckRampBox } from '@fortawesome/free-solid-svg-icons'
+import { faCheckDouble } from '@fortawesome/free-solid-svg-icons'
+library.add(faUserSecret, faChevronDown, faTruckRampBox, faCheckDouble)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(router).mount('#app')
