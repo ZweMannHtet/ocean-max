818a2ca858e10f76fa7258d474c893e77c7600f2
ZweMannHtet <zweman98240@gmail.com>
2023-09-20 03:23:08 +0630

    Create Navigation for all pages

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,24 @@
 <template>
-  <router-view/>
+  <NavBar />
+  <router-view />
 </template>
 
+<script setup>
+import NavBar from "@/components/NavBar.vue";
+</script>
+
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Cabin&family=Poppins:wght@300&family=Raleway:wght@300&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  /* background-color: #2c3e50; */
 }
 </style>
