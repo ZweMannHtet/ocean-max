50d091c517b2981a540d1ae4588edd9fbe1721b0
ZweMannHtet <zweman98240@gmail.com>
2023-10-01 03:08:39 +0630

    add new component for redirect pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,35 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AboutUs from '../views/AboutUs.vue'
+import PropertyForRent from '../views/PropertyForRent.vue'
+import Agents from '../views/Agents.vue'
+import Services from '../views/Services.vue'
 
 const routes = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/aboutus',
+    name: 'About',
+    component: AboutUs
+  },
+  {
+    path: '/propertyforrent',
+    name: 'PropertyForRent',
+    component: PropertyForRent
+  },
+  {
+    path: '/agents',
+    name: 'Agents',
+    component: Agents
+  },
+  {
+    path: '/services',
+    name: 'Services',
+    component: Services
   }
 ]
 
